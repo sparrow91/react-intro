754f110cf95d97a72c2e1a114845c5f8de15f324
abhishekqb <abhisheknath@qburst.com>
2018-12-15 08:46:36 +0530

    tool stuff

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,19 +1,18 @@
+import React from 'react'
+import {render} from 'react-dom'
+
 const ce = React.createElement;
 const MySecondComponent = function(props) {
-  return ce(
-    "div",
-    null,
-    ce("h1", { style: { color: props.color } }, props.title)
-  );
+  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
 };
 const MyFirstComponent = function() {
   return ce(
-    "div",
+    'div',
     null,
-    ce(MySecondComponent, { title: "Game of thrones", color: "peru" }),
-    ce(MySecondComponent, { title: "Game of thrones", color: "peru" }),
-    ce(MySecondComponent, { title: "Game of thrones", color: "peru" }),
-    ce(MySecondComponent, { title: "Game of thrones", color: "peru" })
+    ce(MySecondComponent, { title: 'Game of thrones', color: 'peru' }),
+    ce(MySecondComponent, { title: 'Game of thrones', color: 'peru' }),
+    ce(MySecondComponent, { title: 'Game of thrones', color: 'peru' }),
+    ce(MySecondComponent, { title: 'Game of thrones', color: 'peru' })
   );
 };
-ReactDOM.render(ce(MyFirstComponent), document.getElementById("app"));
+render(ce(MyFirstComponent), document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+    context: __dirname,
+    entry: "./js/ClientApp.jsx",
+    devtool: "cheap-eval-source-maps",
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    stats: {
+        color: true,
+        reasons: true,
+        chunks: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+}
